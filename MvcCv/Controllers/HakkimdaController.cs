1561f766c7c4141e15e5129b852ac16e16016ce1
furkaniscn <furkan.iscn@outlook.com>
2024-07-31 00:22:26 +0300

    HobilerimPageHasBeenCompleted

--- MvcCv/Controllers/HakkimdaController.cs
+++ MvcCv/Controllers/HakkimdaController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
 
 namespace MvcCv.Controllers
 {
@@ -10,9 +14,28 @@ namespace MvcCv.Controllers
     public class HakkimdaController : Controller
     {
         // GET: Hakkimda
+        GenericRepository<TblHakkimda> repo = new GenericRepository<TblHakkimda>();
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var hakkimda = repo.List();
+            return View(hakkimda);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TblHakkimda p)
+        {
+            var t = repo.Find(x => x.ID == 1);
+            t.Ad = p.Ad;
+            t.Soyad = p.Soyad;
+            t.Adres = p.Adres;
+            t.Telefon = p.Telefon;
+            t.Mail = p.Mail;
+            t.Aciklama = p.Aciklama;
+            t.Resim = p.Resim;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
         }
     }
 }
